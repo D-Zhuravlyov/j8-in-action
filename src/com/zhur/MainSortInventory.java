9099457e007b1c8f63260186250b55c2cbe9927b
D-Zhuravlyov <xzhuravlyov@gmail.com>
2017-07-28 23:10:48 +0300

    Adds usage lambda, comparator.comparing, fixes inventoryholder

--- src/com/zhur/MainSortInventory.java
+++ src/com/zhur/MainSortInventory.java
@@ -3,6 +3,7 @@ package com.zhur;
 import com.zhur.comparator.AppleComparator;
 import com.zhur.model.Apple;
 import com.zhur.model.InventoryHolder;
+import org.omg.PortableInterceptor.INACTIVE;
 
 import java.util.Comparator;
 import java.util.List;
@@ -14,16 +15,45 @@ public class MainSortInventory {
         InventoryHolder inventoryHolder = new InventoryHolder();
         List<Apple> inventory = inventoryHolder.getInventory();
 
+        System.out.println("Comparator CLASS");
+        System.out.println("Initial: ");
+        showList(inventory);
         inventory.sort(new AppleComparator());
+        System.out.println("Result:");
+        showList(inventory);
 
+        System.out.println("ANON CLASS");
+        System.out.println("Initial inventory: ");
+        inventory = inventoryHolder.getInventory();
+        showList(inventory);
         inventory.sort(new Comparator<Apple>() {
-            public int compare(Apple a1, Apple a2){
+            public int compare(Apple a1, Apple a2) {
                 return a1.getSize().compareTo(a2.getSize());
             }
         });
+        System.out.println("Result:");
+        showList(inventory);
 
+        System.out.println("LAMBDA");
+        System.out.println("Initial inventory: ");
+        inventory = inventoryHolder.getInventory();
+        showList(inventory);
+        inventory.sort((a1, a2) -> a1.getSize().compareTo(a2.getSize())
+        );
+        System.out.println("Result:");
+        showList(inventory);
 
+        System.out.printf("comparator.comparing: \n");
+        System.out.println("Initial inventory: ");
+        inventory = inventoryHolder.getInventory();
+        Comparator<Apple> c = Comparator.comparing((Apple a) -> a.getSize());
+        inventory.sort(c);
+        System.out.println("Result:");
+        showList(inventory);
 
+    }
 
+    public static void showList(List l) {
+        l.forEach(System.out::println);
     }
 }
--- src/com/zhur/model/InventoryHolder.java
+++ src/com/zhur/model/InventoryHolder.java
@@ -7,15 +7,18 @@ public class InventoryHolder {
 
   private   Apple a1 = new Apple(44, "red");
   private   Apple a2 = new Apple(34, "red");
+  private   Apple a3 = new Apple(66, "red");
+  private   Apple a4 = new Apple(56, "red");
+  private   Apple a5 = new Apple(31, "red");
 
-  private List<Apple> inventory = new ArrayList<>();
-
-    public InventoryHolder() {
-        inventory.add(a1);
-        inventory.add(a2);
-    }
 
     public List<Apple> getInventory(){
+        List<Apple>  inventory = new ArrayList<>();
+        inventory.add(a1);
+        inventory.add(a2);
+        inventory.add(a3);
+        inventory.add(a4);
+        inventory.add(a5);
         return inventory;
     }
 }
