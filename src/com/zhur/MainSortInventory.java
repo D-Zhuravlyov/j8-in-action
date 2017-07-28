fad2637bfe32e62a1d14c60e68ed4a232dea51e5
D-Zhuravlyov <xzhuravlyov@gmail.com>
2017-07-28 23:16:47 +0300

    Adds usage of method reference

--- src/com/zhur/MainSortInventory.java
+++ src/com/zhur/MainSortInventory.java
@@ -3,10 +3,11 @@ package com.zhur;
 import com.zhur.comparator.AppleComparator;
 import com.zhur.model.Apple;
 import com.zhur.model.InventoryHolder;
-import org.omg.PortableInterceptor.INACTIVE;
 
 import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 public class MainSortInventory {
 
@@ -15,14 +16,14 @@ public class MainSortInventory {
         InventoryHolder inventoryHolder = new InventoryHolder();
         List<Apple> inventory = inventoryHolder.getInventory();
 
-        System.out.println("Comparator CLASS");
+        System.out.println("Comparator CLASS \n");
         System.out.println("Initial: ");
         showList(inventory);
         inventory.sort(new AppleComparator());
         System.out.println("Result:");
         showList(inventory);
 
-        System.out.println("ANON CLASS");
+        System.out.println("ANON CLASS \n");
         System.out.println("Initial inventory: ");
         inventory = inventoryHolder.getInventory();
         showList(inventory);
@@ -34,7 +35,7 @@ public class MainSortInventory {
         System.out.println("Result:");
         showList(inventory);
 
-        System.out.println("LAMBDA");
+        System.out.println("LAMBDA \n");
         System.out.println("Initial inventory: ");
         inventory = inventoryHolder.getInventory();
         showList(inventory);
@@ -43,12 +44,21 @@ public class MainSortInventory {
         System.out.println("Result:");
         showList(inventory);
 
-        System.out.printf("comparator.comparing: \n");
-        System.out.println("Initial inventory: ");
+        System.out.println("comparator.comparing: \n");
         inventory = inventoryHolder.getInventory();
-        Comparator<Apple> c = Comparator.comparing((Apple a) -> a.getSize());
+        System.out.println("Initial inventory: ");
+        showList(inventory);
+        Comparator<Apple> c = comparing((Apple a) -> a.getSize());
         inventory.sort(c);
         System.out.println("Result:");
+        showList(inventory);
+
+        System.out.println("Method reference: \n");
+        inventory = inventoryHolder.getInventory();
+        System.out.println("Initial inventory: ");
+        showList(inventory);
+        System.out.println("Result:");
+        inventory.sort(comparing(Apple::getSize));
         showList(inventory);
 
     }
