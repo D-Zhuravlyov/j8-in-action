c648d6135eb7cfd2e70e0a6fe1dd5486dea011ef
D-Zhuravlyov <xzhuravlyov@gmail.com>
2017-08-01 22:41:08 +0300

    Adds first usage of stream to get squares of numbers.

---
+++ streams/src/main/java/numbers/MainFindSquaresOfNumbers.java
@@ -0,0 +1,18 @@
+package numbers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class MainFindSquaresOfNumbers {
+    public static void main(String[] args) {
+
+       List<Integer> numbers = Arrays.asList(1,2,3,4,5,6);
+
+        numbers.stream()
+                .map(n -> n*= n)
+                .collect(toList())
+                .forEach(System.out::println);
+    }
+}
